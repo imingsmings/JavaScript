f614bcb3ade49e5d9198f529baa61f5b652d776c
Jason Wang <imings@icloud.com>
2022-05-06 23:32:31 +0800

    update dir

---
+++ array/test.js
@@ -0,0 +1,12 @@
+class App {
+  state = {
+    count: 1,
+  };
+
+  fun = () => {
+    console.log(123);
+  };
+}
+
+const app = new App();
+console.log(app);
--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-import './style.css';
-
-import './regex/search.js';
+import './flow/switch.js';
