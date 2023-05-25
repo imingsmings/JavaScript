5e224a841d95d025cf486b722be42a75e35ac2da
Jason Wang <imings@icloud.com>
2023-05-25 23:32:54 +0800

    feat: var

---
+++ DOM/properites.js
@@ -0,0 +1,34 @@
+// console.log(ele.innerHTML);
+// console.log(ele.outerHTML);
+
+// ele.outerHTML = `<section id="ele"> </section>`
+// console.log(ele.outerHTML);
+// alert(ele.outerHTML)
+
+// const oDiv = document.querySelector('div')
+// oDiv.outerHTML = '<p>A new element</p>'
+// alert(oDiv.outerHTML)
+
+// const p = document.querySelector("p");
+// console.dir(p); // shows: "P"
+// p.outerHTML = "<div>This div replaced a paragraph.</div>";
+// console.dir(p); // still "P";
+
+// alert(news.textContent)
+
+// setInterval(() => elem.hidden = !elem.hidden, 1000)
+// const elem = document.getElementById('news')
+// elem.firstChild.nextSibling.data = 'comment-1'
+// console.log(elem.firstChild.nextSibling.data);
+
+// for (let li of document.querySelectorAll('li')) {
+//   let title = li.firstChild.data.trim()
+//   let count = li.getElementsByTagName('li').length
+// }
+
+// console.log(document.body.lastChild) // 8
+// let body = document.body;
+// body.innerHTML = "<--" + body.tagName + "-->";
+// console.log(body.firstChild.data); // <--BODY-->
+
+// console.log(document.__proto__);
---
+++ ecma/variable/var.js
@@ -0,0 +1,11 @@
+// var a;
+// console.log(a);
+
+// let b;
+// console.log(b);
+
+var a = 1;
+console.log(globalThis.a);
+console.log(window.a);
+console.log(a)
+// globalThis
