b2414caba238d18ee6d0f0e1aa3be9b34d55efb6
Jason Wang <imings@icloud.com>
2023-05-28 22:27:14 +0800

    variable

---
+++ ecma/variable/let.js
@@ -0,0 +1,13 @@
+// if (1) {
+//     let a = 1;
+// }
+
+// console.log(a);
+
+// console.log(a);
+// let a = 1;
+
+// let a = 1; 
+// {
+//     var a = 2;
+// }
--- ecma/variable/var.js
+++ ecma/variable/var.js
@@ -1,11 +1,28 @@
-// var a;
-// console.log(a);
+// // var a;
+// // console.log(a);
 
-// let b;
-// console.log(b);
+// // let b;
+// // console.log(b);
 
-var a = 1;
-console.log(globalThis.a);
-console.log(window.a);
-console.log(a)
+// var a = 1;
+// console.log(globalThis.a);
+// console.log(window.a);
+// console.log(a)
 // globalThis
+
+// console.log(a);
+// var a = 1;
+
+function test() {
+    a  = 1;
+    var b = 2;
+
+    var c = d = 3;
+
+    console.log(b);
+}
+
+test();
+
+console.log(a);
+console.log(d);
