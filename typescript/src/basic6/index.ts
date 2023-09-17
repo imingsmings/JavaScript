4828fb90351876dc5c234efce8d52b70c7ec370e
Jason Wang <imings@icloud.com>
2023-09-17 14:38:02 +0800

    learn: ts

---
+++ typescript/src/basic6/index.ts
@@ -0,0 +1,31 @@
+// function (a, b): number {
+//     return a + b;
+// }
+
+// function plus(a: number, b: number): number
+// function plus (a: number, b: number) {
+//     return a + b
+// }
+
+// function plus(a: number, b: number): void {
+//     console.log(a + b);
+// }
+
+// function f(n: number, dot?: number) {
+//     console.log(n.toFixed(dot));
+// }
+
+// f(3.1415926)
+// f(3.1415926, 2);
+
+// function testFn (a: number, b?: number) {
+//     console.log(b?.toString())
+// }
+
+// testFn(1, 2)
+// testFn(1)
+
+console.log(void 0 === void(0)); // true
+console.log(void 0 === void 100); // true
+console.log(void 0 === void 'abc'); // true
+console.log(void 0 === undefined); // true
