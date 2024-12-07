5094c29d293662176f9e7032b28e8027a0490ec9
Jason Wang <imings@icloud.com>
2024-12-07 13:20:53 +0800

    XHR

--- bun/index.ts
+++ bun/index.ts
@@ -2,6 +2,13 @@ import express from 'express'
 
 const app = express()
 
+app.use('*', (req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Headers', 'Authorization')
+    next()
+})
+
 app.get('/', (req, res, next) => {
     res.json({
         name: 'bun',
