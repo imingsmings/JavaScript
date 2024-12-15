6b350f698f38396333a0bd8c6cb17d004277fe0a
Jason Wang <imings@icloud.com>
2024-12-15 21:52:13 +0800

    go test

--- goweb/1.js
+++ goweb/1.js
@@ -1,7 +1,15 @@
-fetch('https://127.0.0.1:8001/v1/user/list/5/1', {
-    cors: true
-})
-    .then((res) => res.json())
-    .then((data) => {
-        console.log(data)
-    })
+// fetch('https://127.0.0.1:8001/v1/user/list/5/1', {
+//     cors: true
+// })
+//     .then((res) => res.json())
+//     .then((data) => {
+//         console.log(data)
+//     })
+
+// fetch('https://127.0.0.1:8001/v1/user/one/id/123', {
+//     cors: true
+// })
+//     .then((res) => res.json())
+//     .then((data) => {
+//         console.log(data)
+//     })
