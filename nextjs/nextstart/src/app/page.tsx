46c6ee776ae9d336ee81cbf99cc3152078e0a637
Jason Wang <imings@icloud.com>
2025-03-08 15:33:30 +0800

    nextjs: useRouter

--- nextjs/nextstart/src/app/page.tsx
+++ nextjs/nextstart/src/app/page.tsx
@@ -1,9 +1,19 @@
+'use client'
+
 import NavBar from '@/components/NavBar'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+
+  const navigate = (page: string) => {
+    router.push(`/${page}`)
+  }
+
   return (
     <div>
       <NavBar></NavBar>
+      <button onClick={() => navigate('about')}>Go to about page</button>
     </div>
   )
 }
