01299de8bf531c27e9bff38f1cbe4b27599edb81
Jason Wang <imings@icloud.com>
2025-03-09 15:51:57 +0800

    nextjs: image

--- nextjs/nextstart/src/app/image/page.tsx
+++ nextjs/nextstart/src/app/image/page.tsx
@@ -5,18 +5,22 @@ const ImagePage = () => {
   return (
     <div>
       <Image
+        priority={true}
         src={pic}
         alt=''
-        width={400}
-        height={400}
       />
       <Image
+        loading={'lazy'}
         src={
           'https://images.unsplash.com/photo-1475776408506-9a5371e7a068?q=80&w=3916&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         }
         alt=''
-        width={400}
-        height={400}
+        width={0}
+        height={0}
+        style={{
+          width: '400px',
+          height: 'auto'
+        }}
       />
     </div>
   )
