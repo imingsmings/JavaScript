fa77981cba2f5618e7399ad23d32780a0a667c03
Jason Wang <imings@icloud.com>
2025-03-09 13:53:44 +0800

    nextjs: image

---
+++ nextjs/nextstart/src/app/image/page.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import pic from '../../../public/florian-van-duyn.jpg'
+
+const ImagePage = () => {
+  return (
+    <div>
+      <Image
+        src={pic}
+        alt=''
+        width={400}
+        height={400}
+      />
+      <Image
+        src={
+          'https://images.unsplash.com/photo-1475776408506-9a5371e7a068?q=80&w=3916&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+        }
+        alt=''
+        width={400}
+        height={400}
+      />
+    </div>
+  )
+}
+
+export default ImagePage
