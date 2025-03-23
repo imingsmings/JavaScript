49297c27e1795d8719609302eb758f7c9a15715b
Jason Wang <imings@icloud.com>
2025-03-23 19:44:32 +0800

    react-hook-form

---
+++ react-hook-form-demo/src/App.tsx
@@ -0,0 +1,76 @@
+// import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+interface FormData {
+  name: string
+  email: string
+  password: string
+}
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting }
+  } = useForm<FormData>()
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor='name'>Name: </label>
+        <input
+          type='text'
+          id='name'
+          {...register('name', {
+            required: 'Name is requried'
+          })}
+        />
+        {errors.name && <p style={{ color: 'red' }}>{errors.name.message}</p>}
+      </div>
+      <div>
+        <label htmlFor='email'>Email: </label>
+        <input
+          type='email'
+          id='email'
+          {...register('email', {
+            required: 'Email is requried',
+
+            pattern: {
+              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+              message: 'Invalid email address'
+            }
+          })}
+        />
+        {errors.email && <p style={{ color: 'red' }}>{errors.email.message}</p>}
+      </div>
+      <div>
+        <label htmlFor='password'>Password: </label>
+        <input
+          type='password'
+          id='password'
+          placeholder='Password'
+          {...register('password', {
+            required: 'Password is requried',
+            minLength: {
+              value: 8,
+              message: 'Password must be at least 8 characters'
+            }
+          })}
+        />
+        {errors.password && <p style={{ color: 'red' }}>{errors.password.message}</p>}
+      </div>
+      <button
+        type='submit'
+        disabled={isSubmitting}
+      >
+        {isSubmitting ? 'Loading' : 'Submit'}
+      </button>
+    </form>
+  )
+}
+
+export default App
