7e03ab71ceaeb7274b6b33d83d22ee9f8f0dd48c
Jason Wang <imings@icloud.com>
2025-06-01 21:09:23 +0800

    cos test

---
+++ nodejs/cos/main.js
@@ -0,0 +1,36 @@
+import COS from 'cos-nodejs-sdk-v5'
+import fs from 'node:fs'
+import path from 'node:path'
+
+async function upload(dir, filePath) {
+  const cos = new COS({
+    SecretId: process.env.COS_SECRET_ID,
+    SecretKey: process.env.COS_SECRET_KEY
+  })
+
+  const filename = path.basename(filePath)
+  const Key = `${dir}/${filename}`
+
+  const result = await cos.putObject({
+    Bucket: process.env.COS_BUCKET,
+    Region: process.env.COS_REGION,
+    Key,
+    Body: fs.createReadStream(filePath)
+  })
+
+  if (result.statusCode === 200) {
+    console.log('上传成功')
+  }
+
+  const staticPath = cos.getObjectUrl({
+    Bucket: process.env.COS_BUCKET,
+    Region: process.env.COS_REGION,
+    Key: Key,
+    Sign: false,
+    Domain: 'https://imings.cn/'
+  })
+
+  console.log(staticPath)
+}
+
+upload('nodejs', '/Users/jason/Downloads/orm.png')
